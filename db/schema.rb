f114472dd81920b4d6fe1a695e89d743e32a751f
Hemant soni <sonihemant4741@gmail.com>
2020-09-11 01:59:11 +0530

    vote model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_09_204542) do
+ActiveRecord::Schema.define(version: 2020_09_10_202813) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -30,5 +30,16 @@ ActiveRecord::Schema.define(version: 2020_09_09_204542) do
     t.string "password_digest"
   end
 
+  create_table "votes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "article_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_votes_on_article_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+  end
+
   add_foreign_key "articles", "users"
+  add_foreign_key "votes", "articles"
+  add_foreign_key "votes", "users"
 end
