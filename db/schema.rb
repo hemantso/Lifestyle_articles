aa1ea4f7f03aea5910dfd802249abab88dda7f1b
Hemant soni <sonihemant4741@gmail.com>
2020-09-11 02:11:40 +0530

    category model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_10_202813) do
+ActiveRecord::Schema.define(version: 2020_09_10_204034) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2020_09_10_202813) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.integer "priority"
+    t.integer "article_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_categories_on_article_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,6 +49,7 @@ ActiveRecord::Schema.define(version: 2020_09_10_202813) do
   end
 
   add_foreign_key "articles", "users"
+  add_foreign_key "categories", "articles"
   add_foreign_key "votes", "articles"
   add_foreign_key "votes", "users"
 end
