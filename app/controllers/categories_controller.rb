903e457c320c2f7f713305dcb237a9c39d1cb710
Hemant soni <sonihemant4741@gmail.com>
2020-09-13 20:14:10 +0530

    assosiation between article and category

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+class CategoriesController < ApplicationController
+  before_action :require_admin, except: [:index, :show]
+  def index
+    @categories = Category.all
+    end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+
+      flash[:success] = 'Category was created successfully'
+
+      redirect_to categories_path
+
+    else
+
+      render 'new'
+
+    end
+  end
+
+  def show; end
+
+    private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+  def require_admin
+
+    if !logged_in? || (logged_in? and !current_user)
+    
+    flash[:danger] = "Only admins can perform that action"
+    
+    redirect_to categories_path
+    
+    end
+    
+    end
+  end
