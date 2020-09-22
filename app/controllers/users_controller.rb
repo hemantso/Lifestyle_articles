8689fa1eee7f44b3588e57a194f7345b339b2e1a
Hemant soni <sonihemant4741@gmail.com>
2020-09-22 22:36:29 +0530

    user cannot access page after logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
+  before_action :require_user, only: %i[index show]
   def index
     @users = User.all
   end
